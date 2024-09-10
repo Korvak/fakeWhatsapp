b96d5c0778f89a84b45f0747201bb77a0e142103
Virgil <virgilvancini@gmail.com>
2024-09-11 01:19:01 +0200

    added purge unused styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files in your project
+    './src/**/*.{js,jsx,ts,tsx}', // Include all source files in your project
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}'
+    ],
+  },
 };
